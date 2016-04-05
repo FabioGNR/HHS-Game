085c55ccc395b957cbece3c44199ee393dcd36e0
Fabio Waljaard <fabio.waljaard@gmail.com>
2016-04-05 10:04:37 +0200

    Implemented LevelWriter

--- HHSGame/src/hhsgame/editor/LevelWriter.java
+++ HHSGame/src/hhsgame/editor/LevelWriter.java
@@ -5,10 +5,28 @@
  */
 package hhsgame.editor;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Fabio
  */
 public class LevelWriter {
-    
+    public static void writeLevel(String filepath, String[][] tiles) throws IOException
+    {
+        FileWriter writer = new FileWriter(filepath);
+        for(int l = 0; l < tiles.length; l++) {
+            String[] line = tiles[l];
+            for(int b = 0; b < line.length; b++) {
+                String tile = line[b];
+                writer.append(tile);
+                if(b < line.length) {
+                    writer.append(", ");
+                }
+            }
+            writer.append('\n');    
+        }
+        writer.close();       
+    }
 }
