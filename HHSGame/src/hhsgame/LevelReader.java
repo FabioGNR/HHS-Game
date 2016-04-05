7c3fc8460974bd18b13d6d3fe0f1477d8be94cef
Joris Jelle Willig <Jorisjellewillig@gmail.com>
2016-04-05 12:26:14 +0200

    started a change of reading and storing levels

--- HHSGame/src/hhsgame/LevelReader.java
+++ HHSGame/src/hhsgame/LevelReader.java
@@ -16,7 +16,7 @@ public LevelReader(String fileName) {
         readLevels();
     }
     
-    private void openFile(){
+    private void openFile(String fileName){
         try {
             in = new BufferedReader(new FileReader(fileName));
         } catch(Exception e) {
@@ -26,25 +26,48 @@ public LevelReader(String fileName) {
         }
     }
     
+//    private void readLevels() {
+//        openFile();
+//        String[][] lines = new String[ROWS][COLS];
+//        int counter = 0;
+//        String line;
+//        try {
+//            
+//            while((line = in.readLine()) != null) {
+//                lines[counter] = line.split(",");
+//                counter++;
+//                if(counter == 10) {
+//                    counter = 0;
+//                    levels.add(new Level(lines));
+//                    lines = new String[ROWS][COLS];
+//                    in.readLine();
+//                }
+//            }
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+    
     private void readLevels() {
-        openFile();
+        openFile(fileName);
         String[][] lines = new String[ROWS][COLS];
-        int counter = 0;
+        List<String> levelNames = new ArrayList<String>();
         String line;
         try {
-            
             while((line = in.readLine()) != null) {
-                lines[counter] = line.split(",");
-                counter++;
-                if(counter == 10) {
-                    counter = 0;
-                    levels.add(new Level(lines));
-                    lines = new String[ROWS][COLS];
-                    in.readLine();
+                levelNames.add(line);
+            }
+            for(String level : levelNames) {
+                openFile(level);
+                line = in.readLine();
+                for(int i = 0; i < 10; i++) {
+                    lines[i] = line.split(",");
                 }
+                levels.add(new Level(lines));
+                lines = new String[ROWS][COLS];
             }
         } catch(IOException e) {
-            e.printStackTrace();
+            return;
         }
     }
     
