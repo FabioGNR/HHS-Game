1ba7d6cca6c463a26fd4c3669180ae01c32b1a57
FabioGNR <fabio.waljaard@gmail.com>
2016-04-06 14:28:01 +0200

    Fixed GameBoardTest not testing all situations

--- HHSGame/test/hhsgame/GameBoardTest.java
+++ HHSGame/test/hhsgame/GameBoardTest.java
@@ -1,9 +1,6 @@
 package hhsgame;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -29,16 +26,26 @@ public class GameBoardTest {
      */
     @Test
     public void testMoveCharacterToBarricade() {
+        BoardCoordinate expectedPos;
         //barricade with right key 
         character.setKey(new Key(4));
         board.moveCharacter(Game.MoveDirection.Left);
-        BoardCoordinate newPos = new BoardCoordinate(6,8);
+        expectedPos = new BoardCoordinate(6,8);
         charPos = character.getCurrentTile().getPos();
-        assertEquals("expected: character opens barricade", 0, newPos.compareTo(charPos));
+        assertEquals("expected: character opens barricade", 0, expectedPos.compareTo(charPos));
         
         //barricade with wrong key
+        character.setKey(new Key(5));
         board.moveCharacter(Game.MoveDirection.Left);
-        BoardCoordinate expectedPos = new BoardCoordinate (6,8);
+        charPos = character.getCurrentTile().getPos();
+        expectedPos = new BoardCoordinate (6,8);
+        assertEquals("expected: character has same position", 0, expectedPos.compareTo(charPos));
+        
+        //barricade without key
+        character.setKey(null);
+        board.moveCharacter(Game.MoveDirection.Left);
+        charPos = character.getCurrentTile().getPos();
+        expectedPos = new BoardCoordinate (6,8);
         assertEquals("expected: character has same position", 0, expectedPos.compareTo(charPos));
     }
         
@@ -53,19 +60,29 @@ public class GameBoardTest {
         
     @Test
     public void testMoveCharacterOutOfGameEdge(){
-        //test character doesn't go further than (7,9)
-        board.moveCharacter(Game.MoveDirection.Down);
-        board.moveCharacter(Game.MoveDirection.Down);
-        BoardCoordinate expectedPos = new BoardCoordinate(7,9);
-        charPos = character.getCurrentTile().getPos();
-        assertEquals("expected: character stay within gameboard", 0, expectedPos.compareTo(charPos));       
+        BoardCoordinate expectedPos;
         //test character doesn't go further than (9,9)
-        character.setKey(new Key(4));
+        expectedPos = new BoardCoordinate(9,9);
+        character.setCurrentTile(board.getLevelLayout().get(expectedPos));
+        board.moveCharacter(Game.MoveDirection.Down);
+        charPos = character.getCurrentTile().getPos();
+        assertEquals("expected: character stay within gameboard ( bottom edge )", 0, expectedPos.compareTo(charPos));       
+        //test character doesn't go further than (9,9)
+        expectedPos = new BoardCoordinate(9,9);
+        character.setCurrentTile(board.getLevelLayout().get(expectedPos));
         board.moveCharacter(Game.MoveDirection.Right);
-                board.moveCharacter(Game.MoveDirection.Right);
-        board.moveCharacter(Game.MoveDirection.Right);
-        BoardCoordinate expectedNewPos = new BoardCoordinate(9,9);
-        BoardCoordinate charNewPos = character.getCurrentTile().getPos();
-        assertEquals("expected: character stays within gameboard", 0, expectedNewPos.compareTo(charNewPos));
+        assertEquals("expected: character stays within gameboard ( right edge )", 0, expectedPos.compareTo(charPos));
+        // test character doesn't go lower than (0,0)
+        expectedPos = new BoardCoordinate(0,0);
+        character.setCurrentTile(board.getLevelLayout().get(expectedPos));
+        charPos = character.getCurrentTile().getPos();
+        board.moveCharacter(Game.MoveDirection.Up);
+        assertEquals("expected: character stays within gameboard ( top edge )", 0, expectedPos.compareTo(charPos));
+        // test character doesn't go left of (0,0)
+        expectedPos = new BoardCoordinate(0,0);
+        character.setCurrentTile(board.getLevelLayout().get(expectedPos));
+        charPos = character.getCurrentTile().getPos();
+        board.moveCharacter(Game.MoveDirection.Left);
+        assertEquals("expected: character stays within gameboard ( left edge )", 0, expectedPos.compareTo(charPos));
     }
 }
