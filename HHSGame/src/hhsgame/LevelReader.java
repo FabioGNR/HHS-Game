58bb14227699e439dec7be499bdd64725a060763
FabioGNR <fabio.waljaard@gmail.com>
2016-04-07 15:50:35 +0200

    Commented levelReader

--- HHSGame/src/hhsgame/LevelReader.java
+++ HHSGame/src/hhsgame/LevelReader.java
@@ -10,12 +10,15 @@ public class LevelReader {
     private final String fileName;
     private final List<Level> levels = new ArrayList<>();
     
-    //method readLevels() is used when LevelReader is instantiated and read the specified txt file
+    //method readLevels() is used when LevelReader is instantiated 
+    //and reads the specified txt file ( this file contains the names of all levels )
 public LevelReader(String fileName) {
+        // store the name of the list file and read it's levels
         this.fileName = fileName;
         readLevels();
     }
-
+    
+    // attempts to open a file
     private BufferedReader openLevelFile(String fileName) {
         BufferedReader reader;
         try {
@@ -27,6 +30,7 @@ public LevelReader(String fileName) {
         return reader;
     }
     
+    // attempts to open the level list file and sets it as class member for later use
     private void openFile(String fileName){
         try {
             in = new BufferedReader(new FileReader(fileName));
@@ -36,35 +40,42 @@ public LevelReader(String fileName) {
         }
     }
     
+    // (re)opens the level list file and (re)loads all levels
     public void readLevels() {        
+        // make sure we don't add old levels again by clearing the list
         levels.clear();
+        // opens level list 
         openFile(fileName);
         if(in == null) {
             return;
         }
-        String[][] lines = new String[ROWS][COLS];
+        
         List<String> levelNames = new ArrayList<>();
         String line;
         try {
+            // go over each line in the list and add it to list of level names
             while((line = in.readLine()) != null) {
                 if(!line.isEmpty()) { // ignore empty lines
                     levelNames.add(line);
                 }
             }
+            // attempt to open each found level
             for(String level : levelNames) {   
                 try (BufferedReader reader = openLevelFile(level)) {
+                    String[][] lines = new String[ROWS][COLS];
                     if(reader == null) { // file could not be opened
                         continue;
                     }
+                    // reads the level file and stores its lines
                     for(int i = 0; i < ROWS; i++) {
                         line = reader.readLine();
                         lines[i] = line.split(",");
                     }
+                    // load the level using Level and store it in the list
                     levels.add(new Level(lines, level));
-                    lines = new String[ROWS][COLS];
                 }
             }
-            in.close();
+            in.close(); // closes the level list file because we're done reading from it ( for now )
         } catch(Exception e) {
         }
     }
